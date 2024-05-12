d374cec39f761f3fa489c1196c93a3fa99f78e8d
fishyer <yutianran666@gmail.com>
2024-05-12 10:05:04 +0800

    feat(plugin): add ability to update frontmatter manually
    This commit adds a new command called "Update Frontmatter" to the plugin. Users can now manually trigger the update of frontmatter for the active file. Additionally, it introduces new settings for configuring frontmatter properties such as slug and zhihu-tags.

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,17 @@ import {
 	Setting,
 	TFile,
 	moment,
+	TagCache
 } from "obsidian";
 import { v4 as uuidv4 } from "uuid";
 
 interface FrontmatterModifiedSettings {
-	frontmatterProperty: string;
+	updatedProperty: string;
 	// 添加created_at属性
 	createdProperty: string;
 	uuidProperty: string;
+	slugProperty: string;
+	zhihuTagsProperty: string;
 	momentFormat: string;
 	excludedFolders: string[];
 	useKeyupEvents: boolean;
@@ -24,11 +27,13 @@ interface FrontmatterModifiedSettings {
 }
 
 const DEFAULT_SETTINGS: FrontmatterModifiedSettings = {
-	frontmatterProperty: "updated_at",
+	updatedProperty: "lastmod",
 	// 添加created_at的默认值
-	createdProperty: "created_at",
-	uuidProperty: "uuid",
-	momentFormat: "YYYY-MM-DD HH:mm:ss",
+	createdProperty: "date",
+	uuidProperty: "id",
+	slugProperty: "slug",
+	zhihuTagsProperty:"zhihu-tags",
+	momentFormat: "YYYY-MM-DDTHH:mm:ss+08:00",
 	excludedFolders: [],
 	useKeyupEvents: false,
 	onlyUpdateExisting: false,
@@ -127,6 +132,19 @@ export default class FrontmatterModified extends Plugin {
 				}
 			}
 		});
+
+		//添加一个Command,用于手动更新frontmatter
+		this.addCommand({
+			id: "update-frontmatter",
+			name: "Update Frontmatter",
+			callback: () => {
+				const activeFile = this.app.workspace.getActiveFile();
+				if (activeFile) {
+					this.realUpdateFrontmatter(activeFile);
+				}
+			}
+		});	
+		
 	}
 
 	async loadSettings() {
@@ -141,23 +159,13 @@ export default class FrontmatterModified extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	/**
-	 * Use a timeout to update the metadata only once the user has stopped typing.
-	 * If the user keeps typing, then it will reset the timeout and start again from zero.
-	 *
-	 * Obsidian doesn't appear to correctly handle this situation otherwise, and pops an
-	 * error to say "<File> has been modified externally, merging changes automatically."
-	 *
-	 * @param {TFile} file
-	 */
-	async updateFrontmatter(file: TFile) {
-		clearTimeout(this.timer[file.path]);
-		this.timer[file.path] = window.setTimeout(() => {
-			const cache = this.app.metadataCache.getFileCache(file);
+	async realUpdateFrontmatter(file: TFile) {
+		console.log("realUpdateFrontmatter",file.path);
+		const cache = this.app.metadataCache.getFileCache(file);
 			if (
 				this.settings.onlyUpdateExisting &&
 				!cache?.frontmatter?.hasOwnProperty(
-					this.settings.frontmatterProperty
+					this.settings.updatedProperty
 				)
 			) {
 				// The user has chosen to only update the frontmatter property IF it already exists
@@ -182,9 +190,9 @@ export default class FrontmatterModified extends Plugin {
 						frontmatter[this.settings.appendField] === true;
 					let secondsSinceLastUpdate = Infinity;
 					let previousEntryMoment;
-					if (frontmatter[this.settings.frontmatterProperty]) {
+					if (frontmatter[this.settings.updatedProperty]) {
 						let previousEntry =
-							frontmatter[this.settings.frontmatterProperty];
+							frontmatter[this.settings.updatedProperty];
 						if (isAppendArray && Array.isArray(previousEntry)) {
 							// If we are using an array of updates, get the last item in the list
 							previousEntry =
@@ -210,7 +218,7 @@ export default class FrontmatterModified extends Plugin {
 						if (isAppendArray) {
 							let entries =
 								frontmatter[
-									this.settings.frontmatterProperty
+									this.settings.updatedProperty
 								] || [];
 							if (!Array.isArray(entries)) entries = [entries];
 							// We are using an array of entries. We need to check whether we want to replace the last array
@@ -233,9 +241,29 @@ export default class FrontmatterModified extends Plugin {
 							}
 							newEntry = entries;
 						}
-						frontmatter[this.settings.frontmatterProperty] =
-							newEntry;
-
+						console.log("准备更新frontmatter");
+						console.log(frontmatter);
+						//自动添加tiltle属性,值为文件名,方便hugo使用
+						frontmatter["title"] = file.basename
+						// 自动添加aliases属性,默认值为空数组,方便hugo使用
+						const aliases:string[] = frontmatter["aliases"];
+						console.log(aliases);
+						if (!aliases) {
+							frontmatter["aliases"] = [];
+						}
+						// 自动添加tags属性,值为空数组,方便hugo使用
+						const fileCache = this.app.metadataCache.getFileCache(file);
+						console.log("fileCache.tags: ",fileCache);
+						let tags:TagCache[]=[]
+						if (fileCache) {
+							console.log("fileCache存在,更新tags");
+							tags = fileCache.tags;
+						}else{
+							console.log("fileCache不存在,新建tags");
+							tags = []
+						}
+						console.log("tags: ",tags); 
+						
 						// 更新创建时间的逻辑
 						const createdTime =
 							frontmatter[this.settings.createdProperty];
@@ -244,21 +272,26 @@ export default class FrontmatterModified extends Plugin {
 								file.stat.ctime
 							).format(this.settings.momentFormat);
 						}
+						//更像updatedProperty
+						frontmatter[this.settings.updatedProperty] =newEntry;
 						// 更新uuid的逻辑
 						const uuid = frontmatter[this.settings.uuidProperty];
 						if (!uuid) {
 							frontmatter[this.settings.uuidProperty] = uuidv4();
 						}
-						console.log("准备更新frontmatter");
-						console.log(frontmatter);
-						//自动添加tiltle属性,值为文件名,方便hugo使用
-						frontmatter["title"] = file.basename
-						// 自动添加tags属性,值为空数组,方便hugo使用
-						const tags = frontmatter["tags"];
-						console.log(tags);
-						if (!tags) {
-							frontmatter["tags"] = [];
+						//自动添加slug属性,值为uuid的值,方便hugo使用
+						const slug = frontmatter[this.settings.slugProperty];
+						console.log(slug);
+						if (!slug) {
+							frontmatter[this.settings.slugProperty] = frontmatter[this.settings.uuidProperty]
 						}
+						//自动添加zhihu-tags属性,值为tags的值,方便zhihu插件使用
+						const obsidian_tags = this.getStringTags(tags);
+						console.log("obsidian_tags: ",obsidian_tags);
+						frontmatter["tags"] = obsidian_tags
+						const zhihu_tags = this.getStringTags(tags);
+						console.log("zhihu_tags: ",zhihu_tags);
+						frontmatter[this.settings.zhihuTagsProperty] =zhihu_tags
 						// const published = frontmatter["published"];
 						// if (!published) {
 						// 	// 默认发布
@@ -274,8 +307,38 @@ export default class FrontmatterModified extends Plugin {
 					}
 				});
 			}
+
+	}
+
+	/**
+	 * Use a timeout to update the metadata only once the user has stopped typing.
+	 * If the user keeps typing, then it will reset the timeout and start again from zero.
+	 *
+	 * Obsidian doesn't appear to correctly handle this situation otherwise, and pops an
+	 * error to say "<File> has been modified externally, merging changes automatically."
+	 *
+	 * @param {TFile} file
+	 */
+	async updateFrontmatter(file: TFile) {
+		console.log("updateFrontmatter",file.path);
+		clearTimeout(this.timer[file.path]);
+		this.timer[file.path] = window.setTimeout(() => {
+			this.realUpdateFrontmatter(file);
 		}, this.settings.timeout * 1000);
 	}
+
+	// 写一个拷贝字符串数组的函数
+	 getStringTags(tags: TagCache[]): string[] {
+		const tagsArray:string[] = tags.map((tag) => tag.tag.replace("#", ""));
+		// // 新建一个数组
+		// let newArr = [];
+		// // 遍历原数组
+		// for (let i = 0; i < tagsArray.length; i++) {
+		// 	newArr[i] = tagsArray[i];
+		// }
+		return tagsArray;
+	}
+	
 }
 
 class FrontmatterModifiedSettingTab extends PluginSettingTab {
@@ -298,9 +361,9 @@ class FrontmatterModifiedSettingTab extends PluginSettingTab {
 			.addText((text) =>
 				text
 					.setPlaceholder("updated_at")
-					.setValue(this.plugin.settings.frontmatterProperty)
+					.setValue(this.plugin.settings.updatedProperty)
 					.onChange(async (value) => {
-						this.plugin.settings.frontmatterProperty = value;
+						this.plugin.settings.updatedProperty = value;
 						await this.plugin.saveSettings();
 					})
 			);
@@ -332,6 +395,36 @@ class FrontmatterModifiedSettingTab extends PluginSettingTab {
 					.setValue(this.plugin.settings.uuidProperty)
 					.onChange(async (value) => {
 						this.plugin.settings.uuidProperty = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		
+		new Setting(containerEl)
+			.setName("Frontmatter property for slug_name")
+			.setDesc(
+				"The name of the YAML/frontmatter property to store the slug_name"
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("slug_name")
+					.setValue(this.plugin.settings.slugProperty)
+					.onChange(async (value) => {
+						this.plugin.settings.slugProperty = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Frontmatter property for zhihu-tags")
+			.setDesc(
+				"The name of the YAML/frontmatter property to store the zhihu-tags"
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("zhihu-tags")
+					.setValue(this.plugin.settings.zhihuTagsProperty)
+					.onChange(async (value) => {
+						this.plugin.settings.zhihuTagsProperty = value;
 						await this.plugin.saveSettings();
 					})
 			);
